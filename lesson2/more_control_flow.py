04b54e84db96938ce91c2f3a027923db544f3c85
An Wang <anwang2009@gmail.com>
2024-09-21 14:41:33 -0700

    more control flow

---
+++ lesson2/more_control_flow.py
@@ -0,0 +1,50 @@
+"""
+Lesson 2 extra control flow
+"""
+
+# TODO (2): Create a function that has no arguments
+# that prints "Hello world!" when called.
+
+
+# TODO (3): Create a function that has no arguments
+# that introduces yourself when called.
+
+
+# TODO (4): Create a function that has one integer argument
+# that does the following:
+# If we list all the natural numbers below 10 that
+# are multiples of 3 or 5, we get 3, 5, 6, 9.
+# The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5
+# below the provided argument n. Return this sum.
+
+
+# TODO (5): Create a function that takes one string argument.
+# The provided argument will be a name. When called the
+# function should print "Good job, An!" if called with "An". 
+
+
+# TODO (6): Create a function that takes two integer arguments
+# x and y. The function should return the minimum of the two
+# arguments. 
+
+
+
+# TODO (1): How do you define a "main" function (the desired 
+# entry point of a script) in python? You can refer to past
+# files for examples of this.
+# 
+# In Java, main looks like:
+#    public static void main(String args[]) {}
+# In C, main looks like either of the following:
+#    int main() {}
+#    int main(int argc, char* argv[]) {}
+# 
+# Your completed main function should call the functions
+# you define in the other TODOs.
+#    2. Call the function
+#    3. Call the function
+#    4. Call the function with argument 1000 and print the result
+#    5. Call the function with argument "Grace"
+#    6. Call the function with arguments 2 and 3 and print the result
+
