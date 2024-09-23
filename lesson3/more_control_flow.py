edd1220cc79e316d671c342caf3903183932af2e
graciekinz <graciekinz166@gmail.com>
2024-09-22 20:15:02 -0700

    YOOOOO

--- lesson3/more_control_flow.py
+++ lesson3/more_control_flow.py
@@ -1,14 +1,16 @@
 """
-Lesson 2 extra control flow
+Lesson 3 extra control flow
 """
 
 # TODO (2): Create a function that has no arguments
 # that prints "Hello world!" when called.
-
+def greeting():
+    print("Hello World!")
 
 # TODO (3): Create a function that has no arguments
 # that introduces yourself when called.
-
+def introduction():
+    print("I am Grace!")
 
 # TODO (4): Create a function that has one integer argument
 # that does the following:
@@ -17,16 +19,31 @@ Lesson 2 extra control flow
 # The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5
 # below the provided argument n. Return this sum.
+def multiples_sum(n):
+    addon = 0
+    for i in range(1, n):
+        if i % 3 == 0:
+            addon += i
+        if i % 5 == 0:
+            addon += i
+    return addon
 
 
 # TODO (5): Create a function that takes one string argument.
 # The provided argument will be a name. When called the
 # function should print "Good job, An!" if called with "An". 
-
+def takearg(name):
+    # name = input("What is your name?: ")
+    print("Good job,", name)
 
 # TODO (6): Create a function that takes two integer arguments
 # x and y. The function should return the minimum of the two
 # arguments. 
+def lesser(x, y):
+    if x <= y:
+        return x
+    else:
+        return y
 
 
 
@@ -39,6 +56,14 @@ Lesson 2 extra control flow
 # In C, main looks like either of the following:
 #    int main() {}
 #    int main(int argc, char* argv[]) {}
+if __name__ == "__main__":
+    greeting()
+    introduction()
+    value = multiples_sum(10)
+    print(value)
+    takearg("An")
+    value2 = lesser(11, 10)
+    print(value2)
 # 
 # Your completed main function should call the functions
 # you define in the other TODOs.
