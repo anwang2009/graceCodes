1120d22362318f011a8788dddd3b8e41106f346e
An Wang <anwang2009@gmail.com>
2024-09-29 13:32:47 -0700

    lesson 5 good luck

---
+++ lesson5/strings.py
@@ -0,0 +1,44 @@
+"""
+Lesson 5: Doing stuff with strings
+"""
+
+# TODO 2: Define a function more_consonants that takes a string and
+# returns True if the string has more consonants
+# than vowels.
+
+
+# TODO 3: Define a function is_palindrome that takes a string and
+# returns True if the string is a palindrome,
+# and False otherwise.
+
+
+# TODO 4: Define a function `matches` that takes two strings and
+# returns True if the strings match each other in the following
+# way: '*' represents any single character.
+# a*rdvark matches aardvark
+# hi matches hi
+# *i matches h*
+# ol* doesn't match el*
+# **** matches ****
+# *** doesn't match *
+# Returns False if the strings don't match
+
+
+# TODO 1: Write the main entry point that calls the
+# following functions with a few test cases each:
+# - is_palindrome
+# - more_consonants
+# - matches
+# 
+# Hint: You can print the result of each test, or you can use
+# the keyword `assert`, which expects the expression after it to
+# evaluate to True
+# 
+# Printing:
+#    result = is_palindrome("racecar")
+#    print("is_palindrome of racecar is", result)
+#
+# Asserting:
+#    assert is_palindrome("racecar")
+#    assert not is_palindrome("not a palindrome haha")
+
