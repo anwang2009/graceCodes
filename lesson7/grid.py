f085cf82a2bdb438bb5e857ad63be33c78e7fde7
graciekinz <graciekinz166@gmail.com>
2024-10-09 20:09:20 -0700

    EFFICIENCY FTW

--- lesson7/grid.py
+++ lesson7/grid.py
@@ -4,7 +4,18 @@ Lesson 7: Representing 2D grids
 
 # How would you represent a 2D grid in Python?
 # Assume each spot in the grid is either empty or "X".
-
+def grid(rows, columns):
+    X = "X"
+    blank = "."
+    for i in range(rows): #Rows
+        for j in range(columns): #Stuff in lines for odd rows
+            if (i+j) % 2 == 0: #odd
+                print(X, end="")
+            else: #even
+                print(blank, end="")
+        print()
 # How would you print out the 2D grid?
 
 # Code it with a main function
+if __name__ == "__main__":
+    grid(4, 4)
