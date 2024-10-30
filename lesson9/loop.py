2163b9421ffb46f89a5f0e103521efa1f73a317f
An Wang <anwang2009@gmail.com>
2024-10-30 11:26:07 -0700

    lesson 9

---
+++ lesson9/loop.py
@@ -0,0 +1,40 @@
+# Lesson 9: Loop practice
+
+def exercise01():
+    # TODO: print the first 10 natural numbers using a while loop
+    raise NotImplementedError()
+
+
+def exercise02():
+    # TODO: write a program to print the following number pattern
+    # using a loop:
+    # 1
+    # 1 2
+    # 1 2 3
+    # 1 2 3 4
+    # 1 2 3 4 5
+    raise NotImplementedError()
+
+
+def exercise03():
+    # TODO: Calculate the sum of all numbers from 1 to a given number.
+    # Get the number from user input and print the sum.
+    raise NotImplementedError()
+
+
+def exercise04():
+    # TODO: Print the multiplication table up to 10 of a given number.
+    # Get the number from user input.
+    raise NotImplementedError()
+
+def exercise05():
+    # TODO: Print numbers that are divisible by 5 and less than or equal to 150.
+    # from the list below. If the number is greater than 500, stop the loop.
+    numbers = [12, 75, 150, 180, 145, 525, 50]
+    raiseNotImplementedError()
+
+
+if __name__ == "__main__":
+    # Every time you finish an exercise, modify the following
+    # function call to see if the output is correct.
+    exercise01()
