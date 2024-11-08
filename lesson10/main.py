e42cfc5cdf7251936648e9567a6548af9c401818
An Wang <anwang2009@gmail.com>
2024-11-07 19:01:28 -0800

    lesson 10

---
+++ lesson10/main.py
@@ -0,0 +1,21 @@
+"""
+Lesson 10: short functions
+"""
+
+def index_of(some_list, element):
+   # This function should return the index of the first item
+   # in the list that matches the given `element` argument.
+   # If there are no items that equal `element,
+   # return -1.
+   raise NotImplementedError()
+   
+
+
+def fibonacci(n):
+   # This function should print the first n fibonacci numbers
+   raise NotImplementedError()
+
+
+
+if __name__ == "__main__":
+    pass
