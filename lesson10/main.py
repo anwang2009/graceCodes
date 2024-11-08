03d2f3c5b41aa94c001bc3bf902408d89c36914c
graciekinz <graciekinz166@gmail.com>
2024-11-07 20:06:34 -0800

    WAAAAAAAAAAHHHHHHHHHHHHHH

--- lesson10/main.py
+++ lesson10/main.py
@@ -7,15 +7,35 @@ def index_of(some_list, element):
    # in the list that matches the given `element` argument.
    # If there are no items that equal `element,
    # return -1.
-   raise NotImplementedError()
+   for i in range(len(some_list)):
+      if some_list[i] == element:
+         return i
+   return -1
    
 
 
 def fibonacci(n):
    # This function should print the first n fibonacci numbers
-   raise NotImplementedError()
-
+   x = 1
+   pastx = 0
+   temporarystorage = pastx
+   for i in range(n):
+      print(x)
+      temporarystorage = pastx
+      pastx = x
+      x = temporarystorage + x
 
 
 if __name__ == "__main__":
-    pass
+    fdsa = index_of([1, 2, 3, 3, 5], 3)
+    try:
+        print([1, 2, 2, 3, 5].index(10))
+    except Exception as e:
+        print("Caught error", e)
+    fibonacci(10)
+    x = 5
+    z = x
+    y = 6
+    x = y
+    y = z
+    print(x, y)
