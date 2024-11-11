917a7f2a0dea2ac6b693eca747ebc3c3c38e65c6
An Wang <anwang2009@gmail.com>
2024-11-10 18:52:13 -0800

    lesson 11

---
+++ lesson11/main.py
@@ -0,0 +1,26 @@
+"""
+Lesson 11: Guessing game
+"""
+import random
+
+
+def min(nums):
+    # Warmup: return the minimum number in the list of numbers
+    raise NotImplementedError()
+
+
+def index_min(nums):
+    # Warmup: return the index of the minimum number in the list of numbers
+    raise NotImplementedError()
+
+
+
+def game():
+    # Implement a guessing game, where the user has to guess a number between
+    # 1 and 100. Print "higher" or "lower" when the user guesses wrong, until
+    # they get the right answer!
+    target = random.randint(1, 100)
+
+
+if __name__ == "__main__":
+    # your stuff here
