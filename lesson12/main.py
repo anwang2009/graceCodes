f53c7c2d80a6395850be836c824f73310f57c594
An Wang <anwang2009@gmail.com>
2024-11-13 17:55:24 -0800

    lesson 12

---
+++ lesson12/main.py
@@ -0,0 +1,20 @@
+"""
+Lesson 12: Combining lists
+"""
+
+# Write a function that concatenates two lists and returns
+# the resulting list. For example:
+# ["a","b","c"], [1,2,3] → ["a","b","c",1,2,3]
+
+
+# Write a function that combines two lists by alternatingly
+# taking elements, e.g. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+# If the length of the lists is not the same, raise a ValueError
+
+# Extra: Write a function that merges two sorted lists into a
+# new sorted list. For example,
+# [1,4,6],[2,3,5] → [1,2,3,4,5,6]
+
+if __name__ == "__main__":
+    # Your code here
+    pass
