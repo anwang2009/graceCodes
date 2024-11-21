5484f9f0b541e5a0f661c3e9d230f241c46955c8
An Wang <anwang2009@gmail.com>
2024-11-20 16:14:29 -0800

    lesson 13

---
+++ lesson13/main.py
@@ -0,0 +1,20 @@
+"""
+Lesson 13: 2D arrays
+"""
+
+# Warmup: Write a function that prints the first n prime numbers, where n
+# is given as an argument
+
+
+# Write a function "init_2D" that constructs and returns a list that contains three lists.
+# Each inner list has three numbers. Each number from 1 to 9 inclusive should be
+# represented.
+
+
+# Write a function "print_2D" that displays a given argument "list2D".
+# Python will print [['x', 'y', ['x','y'], ['x', 'y']] as [[x, y], [x, y], [x, y]] but
+# we want the output
+# xy
+# xy
+# xy
+
