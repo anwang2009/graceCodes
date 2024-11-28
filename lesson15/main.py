c012244ea84710005bfbbf9ca933ca6f6f7c8837
graciekinz <graciekinz166@gmail.com>
2024-11-27 19:53:01 -0800

    13+1=15?!? Math ain't mathing

--- lesson15/main.py
+++ lesson15/main.py
@@ -2,17 +2,21 @@
 Lesson 15: Classes
 """
 
-
 class Pet:
     def __init__(self, name):
         self.name = name
+        self.count = 0
 
     def talk(self):
-        print("my name is", self.name)
+        print("My name is", self.name, end = ".\n")
+        self.count += 1
 
+    def getCount(self):
+        print(self.count)
 
 
 if __name__ == "__main__":
-    p = Pet()
+    p = Pet("Grace")
     p.talk()
+    p.getCount()
 
